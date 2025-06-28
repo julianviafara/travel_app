5d28a6b5f65df429a72494cca89c2f9c44f7b336
Juli <viafarajulian10@gmail.com>
2025-06-27 22:19:42 -0500

    Implement the tripcard

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -35,8 +35,17 @@ const Dashboard= () =>{
                 <h1 className="text-xl font-semibold text-dark-100">
                     Created Trips</h1>
                 <div className="trip-grid">
-                    {allTrips.slice(0, 4).map((trip) => (
-                        <TripCard />
+                    {allTrips.slice(0, 4).map(({id,name, imageUrls,
+                                               itinerary, tags, estimatedPrice}) => (
+                        <TripCard
+                            key={id}
+                            id={id.toString()}
+                            name = {name}
+                            imageUrl={imageUrls[0]}
+                            location={itinerary?.[0]?.location ?? ''}
+                            tags={tags}
+                            price={estimatedPrice}
+                        />
                     ))}
                 </div>
             </section>
